08ec62af45572da29284253d65a336240600258e
kp-nagar <kumarpal.nagar@gmail.com>
2023-05-13 23:13:16 +0530

    add file upload endpoint.

--- app.py
+++ app.py
@@ -24,3 +24,15 @@ async def index(request: Request):
         return templates.TemplateResponse("index.html", {"request": request})
     except Exception as e:
         print(e)
+
+
+@app.post("/uploadfile")
+async def create_upload_file(file: UploadFile = File(None)):
+    try:
+        logger.info(f"saving file: {file.filename}")
+        with open(file.filename, 'wb') as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        logger.info(f"saved file: {file.filename}")
+        return {'filename': file.filename}
+    finally:
+        file.file.close()
